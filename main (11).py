e063ce8b586e0eb3c2c6700a6d708257e0021a95
Senerrino <celsenerrino@gmail.com>
2026-03-14 22:08:56 -0300

    Add files via upload

---
+++ main (11).py
@@ -0,0 +1,17 @@
+A, B, C = map(float, input().split())
+
+pi = 3.14159
+
+# cálculos
+triangulo = (A * C) / 2
+circulo = pi * (C ** 2)
+trapezio = ((A + B) * C) / 2
+quadrado = B ** 2
+retangulo = A * B
+
+# saída
+print(f"TRIANGULO: {triangulo:.3f}")
+print(f"CIRCULO: {circulo:.3f}")
+print(f"TRAPEZIO: {trapezio:.3f}")
+print(f"QUADRADO: {quadrado:.3f}")
+print(f"RETANGULO: {retangulo:.3f}")
